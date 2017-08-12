c2a88135db379edabca7643451434b0332fe3752
doshisahil <sahil000005@gmail.com>
2017-08-12 13:16:49 +0530

    Xcode Project - Static Library

---
+++ ObjectiveCUtility/ObjectiveCUtility.h
@@ -0,0 +1,13 @@
+//
+//  ObjectiveCUtility.h
+//  ObjectiveCUtility
+//
+//  Created by Home on 12/08/17.
+//  Copyright © 2017 SAHIRAL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectiveCUtility : NSObject
+
+@end
---
+++ ObjectiveCUtility/ObjectiveCUtility.m
@@ -0,0 +1,13 @@
+//
+//  ObjectiveCUtility.m
+//  ObjectiveCUtility
+//
+//  Created by Home on 12/08/17.
+//  Copyright © 2017 SAHIRAL. All rights reserved.
+//
+
+#import "ObjectiveCUtility.h"
+
+@implementation ObjectiveCUtility
+
+@end
